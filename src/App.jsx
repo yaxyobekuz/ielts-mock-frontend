0f14a75406d1c747db06fee7cc3d865b213f53b4
yaxyobekuz <pubgn9642@gmail.com>
2025-07-16 22:05:35 +0500

    initial commir

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// Router
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+// Layouts
+import MainLayout from "./layouts/MainLayout";
+
+// Pages
+import Home from "./pages/Home";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
