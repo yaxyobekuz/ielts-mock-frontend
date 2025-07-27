4d05363051ea66f04e05f3930923634ec0ad9a6c
yaxyobekuz <pubgn9642@gmail.com>
2025-07-27 09:23:38 +0500

    Add TextDraggable component for rendering draggable text and answers

---
+++ src/components/questions/TextDraggable.jsx
@@ -0,0 +1,34 @@
+import { convertToHtml } from "../../lib/helpers";
+
+const TextDraggable = ({ text, initialNumber, answers }) => {
+  return (
+    <div className="flex gap-5 w-full">
+      {/* Questions */}
+      <pre
+        dangerouslySetInnerHTML={{ __html: convertToHtml(text, initialNumber) }}
+        className="flex-1 font-[inherit] overflow-auto prose prose-sm max-w-max"
+      />
+
+      {/* Answers wrapper */}
+      <div>
+        {/* Title */}
+        <b>{answers.title}</b>
+
+        {/* Answers */}
+        <ul className="max-w-max rounded-md space-y-2 p-2 bg-gray-50">
+          {answers.data.map((item, index) => (
+            <li
+              draggable
+              key={index}
+              className="max-w-max bg-white px-2 rounded border border-gray-400 cursor-move"
+            >
+              {item.answer}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TextDraggable;
