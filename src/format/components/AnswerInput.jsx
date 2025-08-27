bb94c437c64e052da419aad541f66972799d5478
yaxyobekuz <pubgn9642@gmail.com>
2025-08-27 18:47:29 +0500

    Add AnswerInput component writing styles

--- src/format/components/AnswerInput.jsx
+++ src/format/components/AnswerInput.jsx
@@ -1,44 +1,29 @@
 // Lodash
 import { debounce } from "lodash";
 
-// Icons
-import { Trash } from "lucide-react";
-
 // Tip Tap
 import { NodeViewWrapper } from "@tiptap/react";
 
 // React
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-const AnswerInput = ({
-  editor,
-  getPos,
-  deleteNode,
-  initialNumber = 1,
-  allowActions = true,
-}) => {
+const AnswerInput = ({ editor, getPos, initialNumber = 1 }) => {
+  const hiddenTextRef = useRef(null);
+  const [text, setText] = useState("");
+  const [inputWidth, setInputWidth] = useState("max");
   const [inputIndex, setInputIndex] = useState(initialNumber);
 
   const calculateIndex = useCallback(() => {
-    try {
-      let index = initialNumber;
-      const currentPos = getPos();
+    let index = initialNumber;
+    const currentPos = getPos();
 
-      editor.state.doc.descendants((node, pos) => {
-        if (node.type.name === "answer-input" && pos < currentPos) {
-          index++;
-        }
-      });
+    editor.state?.doc?.descendants((node, pos) => {
+      if (node.type.name === "answer-input" && pos < currentPos) {
+        index++;
+      }
+    });
 
-      setInputIndex(index);
-
-      if (!allowActions) return;
-      window.dispatchEvent(
-        new CustomEvent("addAnswerInput", { detail: index })
-      );
-    } catch (error) {
-      console.warn("Error calculating input index:", error);
-    }
+    setInputIndex(index);
   }, [editor, getPos]);
 
   useEffect(() => {
@@ -52,45 +37,27 @@ const AnswerInput = ({
     };
   }, [calculateIndex, editor]);
 
-  const handleDeleteNode = () => {
-    if (!allowActions) return;
-    deleteNode();
-    window.dispatchEvent(
-      new CustomEvent("deleteAnswerInput", { detail: inputIndex })
-    );
-  };
-
-  if (!allowActions) {
-    return (
-      <NodeViewWrapper className="inline-block px-1 py-px">
-        <input
-          type="text"
-          placeholder={inputIndex}
-          className="answer-input pr-5"
-          id={`answer-input-${inputIndex}`}
-        />
-      </NodeViewWrapper>
-    );
-  }
+  useEffect(() => {
+    if (hiddenTextRef.current) {
+      setInputWidth(hiddenTextRef.current.offsetWidth + 20);
+    }
+  }, [text]);
 
   return (
-    <NodeViewWrapper className="inline-block px-1 py-px">
-      <div className="flex items-center gap-1.5 relative">
-        <input
-          type="text"
-          placeholder={inputIndex}
-          className="answer-input pr-5"
-          id={`answer-input-${inputIndex}`}
-        />
-        <button
-          title="Delete input"
-          aria-label="Delete input"
-          onClick={handleDeleteNode}
-          className="flex items-center justify-center size-6 absolute right-0"
-        >
-          <Trash color="red" size={16} />
-        </button>
-      </div>
+    <NodeViewWrapper className="inline-block px-1 py-px max-w-full">
+      <span ref={hiddenTextRef} className="hidden-text">
+        {text}
+      </span>
+
+      <input
+        type="text"
+        value={text}
+        className="answer-input"
+        placeholder={inputIndex}
+        style={{ width: inputWidth }}
+        id={`answer-input-${inputIndex}`}
+        onChange={(e) => setText(e.target.value)}
+      />
     </NodeViewWrapper>
   );
 };
