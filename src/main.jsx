095ea0ced4266cf5844225365e2d626f10d3f36f
yaxyobekuz <pubgn9642@gmail.com>
2025-08-27 15:34:04 +0500

    Add redux (store) provider

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,13 @@ import "./styles/index.css";
 // Components
 import App from "./App.jsx";
 
+// Store (Redux)
+import store from "./store";
+import { Provider } from "react-redux";
+
 // Render the project
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
