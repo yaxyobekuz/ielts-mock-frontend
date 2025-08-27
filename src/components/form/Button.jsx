092bf673ca5a36e2055874ed430184b2841ec313
yaxyobekuz <pubgn9642@gmail.com>
2025-08-27 15:34:19 +0500

    Add form button component

---
+++ src/components/form/Button.jsx
@@ -0,0 +1,26 @@
+const Button = ({
+  onClick,
+  children,
+  className = "",
+  type = "button",
+  variant = "primary",
+  ...props
+}) => {
+  const variants = {
+    neutral: "bg-gray-100 hover:bg-gray-200 disabled:!bg-gray-100",
+    primary: "bg-blue-500 text-white hover:bg-blue-600 disabled:!bg-blue-500",
+    danger: "bg-red-500 text-white hover:bg-red-600 disabled:!bg-red-500",
+  };
+
+  return (
+    <button
+      {...props}
+      type={type}
+      onClick={onClick}
+      children={children}
+      className={`flex items-center justify-center h-10 px-5 rounded-lg ${variants[variant]} ${className} transition-colors duration-200 disabled:opacity-70 disabled:cursor-not-allowed`}
+    />
+  );
+};
+
+export default Button;
