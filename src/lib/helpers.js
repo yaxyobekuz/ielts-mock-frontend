ff302fa34edac2f8ecc5f9b2b96d804ba7674b00
yaxyobekuz <pubgn9642@gmail.com>
2025-08-27 15:38:12 +0500

    Remove convertToHtml helper function & add isNumber function

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,29 +1,23 @@
-export const convertToHtml = (text, initialNumber = 1) => {
-  let inputCounter = initialNumber;
+// Check if string is a valid number
+export const isNumber = (string) => {
+  // Check if input is string
+  if (typeof string !== "string") return false;
 
+  // Check if string is empty or only whitespace
+  if (string.trim() === "") return false;
+
+  // Use Number() and isNaN() to check validity
+  return !isNaN(Number(string));
+};
+
+export const countSpecificCharacter = (text, char) => {
+  return text.split(char).length - 1;
+};
+
+export const countExactMatches = (text, target) => {
   return (
-    text
-      // Convert bold
-      .replace(/\*(.*?)\*/g, "<b>$1</b>")
-      // Convert italic
-      .replace(/_(.*?)_/g, "<i>$1</i>")
-      // Convert underline
-      .replace(/\|(.*?)\|/g, "<u>$1</u>")
-      // Convert special input with counter
-      .replace(/\^(?!\w)/g, () => {
-        const currentNumber = inputCounter++;
-        return `<span class="question-input-wrapper"><span class="hidden-text"></span><input type="text" style="width:128px;" class="question-input" placeholder="${currentNumber}" data-number="${currentNumber}"></span>`;
-      })
-      // Convert special draggable input with counter
-      .replace(/\~(?!\w)/g, () => {
-        const currentNumber = inputCounter++;
-        return `<span class="answer-dropzone" data-number="${currentNumber}">${currentNumber}</span>`;
-      })
-      // Convert list items
-      .replace(/^- (.*$)/gim, "<li>$1</li>")
-      // Wrap consecutive list items in <ul>
-      .replace(/(<li>.*<\/li>)/gs, (match) => {
-        return `<ul class="list-disc list-inside -space-y-5">${match}</ul>`;
-      })
-  );
+    text?.match(
+      new RegExp(target.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), "g")
+    ) || []
+  ).length;
 };
