06fc307bebdfc7c62fc75ca9e8b4a53fe2250d90
yaxyobekuz <pubgn9642@gmail.com>
2025-08-27 15:40:44 +0500

    Add AnswerInput node

---
+++ src/format/nodes/AnswerInputNode.jsx
@@ -0,0 +1,40 @@
+// Components
+import AnswerInput from "../components/AnswerInput";
+
+// Tip Tap
+import { Node, mergeAttributes } from "@tiptap/core";
+import { ReactNodeViewRenderer } from "@tiptap/react";
+
+const AnswerInputNode = (initialNumber = 1, allowActions = true) => {
+  return Node.create({
+    inline: true,
+    group: "inline",
+    name: "answer-input",
+
+    parseHTML() {
+      return [{ tag: "input[data-name='answer-input']" }];
+    },
+
+    renderHTML({ HTMLAttributes }) {
+      return [
+        "input",
+        mergeAttributes(HTMLAttributes, {
+          type: "text",
+          "data-name": "answer-input",
+        }),
+      ];
+    },
+
+    addNodeView() {
+      return ReactNodeViewRenderer((props) => (
+        <AnswerInput
+          {...props}
+          allowActions={allowActions}
+          initialNumber={initialNumber}
+        />
+      ));
+    },
+  });
+};
+
+export default AnswerInputNode;
