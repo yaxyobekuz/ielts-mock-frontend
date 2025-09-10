eb98348aa060562741f7b54ff8ebbba88a0dc403
yaxyobekuz <pubgn9642@gmail.com>
2025-09-10 20:16:49 +0500

    Add links api

---
+++ src/api/links.api.js
@@ -0,0 +1,9 @@
+import api from "./api";
+
+export const linksApi = {
+  get: async () => await api.get("/api/links?mine=true"),
+  getById: async (id) => await api.get(`/api/links/${id}`),
+  create: async (data) => await api.post("/api/links", data),
+  preview: async (id) => await api.get(`/api/links/${id}/preview`),
+  addUsage: async (id, data) => await api.post(`/api/links/${id}/usage`, data),
+};
