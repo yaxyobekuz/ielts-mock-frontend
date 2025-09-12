1deb73b3b86d43433b3df6da55b6fe828c3f4ab7
yaxyobekuz <pubgn9642@gmail.com>
2025-09-12 21:19:30 +0500

    Add description to ErrorContent component

--- src/components/ErrorContent.jsx
+++ src/components/ErrorContent.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 // Animated stickers
 import duckSadOutSticker from "@/assets/animated/duck-sad-out.json";
 
-const ErrorContent = ({ error, link }) => (
+const ErrorContent = ({ error = {}, link }) => (
   <div className="flex flex-col items-center justify-center gap-8 w-full h-screen">
     <Lottie
       width="160"
@@ -15,9 +15,15 @@ const ErrorContent = ({ error, link }) => (
       className="size-40"
       animationData={duckSadOutSticker}
     />
-    <h1 className="max-w-sm text-center text-2xl font-semibold">
-      {error.code && error.message ? error.message : "Nimadir xato ketdi"}
-    </h1>
+
+    <div className="max-w-sm space-y-3 text-center">
+      <h1 className="max-w-sm text-center text-2xl font-semibold">
+        {error.code && error.message ? error.message : "Nimadir xato ketdi"}
+      </h1>
+
+      {error.details && <p className="text-gray-500">{error.details}</p>}
+    </div>
+
     {link && (
       <Link
         to={link.url}
