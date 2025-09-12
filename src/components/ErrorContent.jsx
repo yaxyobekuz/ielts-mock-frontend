8354643b838277c6b47da45a9f66db0bf9964958
yaxyobekuz <pubgn9642@gmail.com>
2025-09-12 18:50:49 +0500

    Add ErrorContent component

---
+++ src/components/ErrorContent.jsx
@@ -0,0 +1,32 @@
+// Lottie
+import Lottie from "lottie-react";
+
+// Router
+import { Link } from "react-router-dom";
+
+// Animated stickers
+import duckSadOutSticker from "@/assets/animated/duck-sad-out.json";
+
+const ErrorContent = ({ error, link }) => (
+  <div className="flex flex-col items-center justify-center gap-8 w-full h-screen">
+    <Lottie
+      width="160"
+      height="160"
+      className="size-40"
+      animationData={duckSadOutSticker}
+    />
+    <h1 className="max-w-sm text-center text-2xl font-semibold">
+      {error.code && error.message ? error.message : "Nimadir xato ketdi"}
+    </h1>
+    {link && (
+      <Link
+        to={link.url}
+        className="px-5 py-2 rounded-xl bg-blue-500 text-white transition-colors duration-200 hover:bg-blue-400"
+      >
+        {link.name}
+      </Link>
+    )}
+  </div>
+);
+
+export default ErrorContent;
