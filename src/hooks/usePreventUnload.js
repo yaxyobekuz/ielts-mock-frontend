bb4cfc4a7854631d06f73f55194a721f403b6722
yaxyobekuz <pubgn9642@gmail.com>
2025-09-12 21:18:02 +0500

    Add usePreventUnload hook

---
+++ src/hooks/usePreventUnload.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const usePreventUnload = () => {
+  useEffect(() => {
+    const handleBeforeUnload = (e) => {
+      e.preventDefault();
+      e.returnValue = "";
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+};
+
+export default usePreventUnload;
