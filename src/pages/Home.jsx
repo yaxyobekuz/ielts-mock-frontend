5516bb5c92a04bfc1118fac4661c5d08e3c2d218
yaxyobekuz <pubgn9642@gmail.com>
2025-09-18 21:54:54 +0500

    Remove inter font style from homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gradient-to-br font-Inter from-slate-50 to-blue-50">
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-blue-50">
       {/* Custom Container Styles */}
       <style jsx>{`
         .container {
