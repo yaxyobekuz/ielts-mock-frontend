16ddd738229daa0cf79b546f88ba1df2c9d9fa23
yaxyobekuz <pubgn9642@gmail.com>
2025-09-19 16:41:33 +0500

    Remove unnecessary data parameter

--- src/api/links.api.js
+++ src/api/links.api.js
@@ -5,5 +5,5 @@ export const linksApi = {
   getById: async (id) => await api.get(`/api/links/${id}`),
   create: async (data) => await api.post("/api/links", data),
   preview: async (id) => await api.get(`/api/links/${id}/preview`),
-  addUsage: async (id, data) => await api.post(`/api/links/${id}/usage`, data),
+  addUsage: async (id) => await api.post(`/api/links/${id}/usage`),
 };
