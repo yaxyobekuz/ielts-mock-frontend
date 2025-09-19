52f627dac5c55e83095901a5f7a707b1efcbfe42
yaxyobekuz <pubgn9642@gmail.com>
2025-09-19 16:42:25 +0500

    Remove Form content from Link page

--- src/pages/Link.jsx
+++ src/pages/Link.jsx
@@ -1,20 +1,16 @@
-// React
-import { useEffect } from "react";
-
 // Lottie
 import Lottie from "lottie-react";
 
+// React
+import { useEffect } from "react";
+
 // Api
 import { linksApi } from "@/api/links.api";
-
-// Icons
-import { ChevronRight } from "lucide-react";
 
 // Toast
 import { toast } from "@/notification/toast";
 
 // Form components
-import Input from "@/components/form/Input";
 import Button from "@/components/form/Button";
 
 // Hooks
@@ -25,19 +21,24 @@ import useObjectState from "@/hooks/useObjectState";
 // Page components
 import ErrorContent from "../components/ErrorContent";
 
-// Router
-import { useNavigate, useParams } from "react-router-dom";
-
 // Animated stickers
+import coolSticker from "@/assets/animated/cool.json";
 import hashtagOutSticker from "@/assets/animated/hashtag-out.json";
+
+// Router
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 
 const Link = () => {
   const { linkId } = useParams();
+  const { updateProperty, getProperty } = useStore("start");
   const { link, error, isLoading, setField } = useObjectState({
     link: null,
     error: null,
     isLoading: true,
   });
+
+  const testId = getProperty("testId");
+  const isStarted = getProperty("isStarted");
 
   const loadLink = () => {
     setField("error", null);
@@ -60,12 +61,13 @@ const Link = () => {
   };
 
   useEffect(() => {
-    !link && loadLink();
+    !link && !isStarted && loadLink();
   }, []);
 
+  if (isStarted) return <Navigate to={`/tutorial/${testId}`} />;
   if (isLoading) return <LoadingContent />;
   if (error) return <ErrorContent error={error} />;
-  return <FormContent link={link} linkId={linkId} />;
+  return <StartContent updateStart={updateProperty} linkId={linkId} />;
 };
 
 const LoadingContent = () => (
@@ -80,32 +82,25 @@ const LoadingContent = () => (
   </div>
 );
 
-const FormContent = ({ linkId }) => {
+const StartContent = ({ linkId, updateStart }) => {
   const navigate = useNavigate();
   const { setModule } = useModule();
-  const { updateProperty } = useStore("userInfo");
-  const { name, age, phone, isLoading, setField } = useObjectState({
-    name: "",
-    age: "",
-    phone: "",
-    isLoading: false,
-  });
+  const { isLoading, setField } = useObjectState({ isLoading: false });
 
-  const handleSubmit = (e) => {
+  const handleStartTest = (e) => {
     e.preventDefault();
     if (isLoading) return;
     setField("isLoading", true);
 
     linksApi
-      .addUsage(linkId, { age, name, phone })
+      .addUsage(linkId)
       .then(({ code, test }) => {
         if (code !== "usageAdded" || !test) throw new Error();
 
-        // Store user info
-        updateProperty("age", age);
-        updateProperty("name", name);
-        updateProperty("phone", phone);
-        updateProperty("linkId", linkId);
+        // Store start data
+        updateStart("linkId", linkId);
+        updateStart("isStarted", true);
+        updateStart("testId", test._id);
 
         // Update test data
         setModule(test.reading?.parts, test._id, "reading");
@@ -122,82 +117,21 @@ const FormContent = ({ linkId }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center gap-8 w-full h-screen px-5">
-      <div className="relative max-w-md w-full bg-gray-50 p-6 rounded-3xl overflow-hidden">
-        <div
-          className={`${
-            isLoading ? "opacity-50 select-none" : "opacity-100"
-          } w-full transition-opacity duration-200`}
-        >
-          {/* Title */}
-          <h1 className="mb-1.5 text-center text-xl font-semibold">
-            Ma'lumotlarni to'ldiring
-          </h1>
+    <div className="flex flex-col items-center justify-center gap-8 w-full h-screen">
+      <Lottie
+        width="160"
+        height="160"
+        className="size-40"
+        animationData={coolSticker}
+      />
 
-          {/* Description */}
-          <p className="mb-3.5 text-center text-gray-500 text-[17px]">
-            Biz sizning ma'lumotlaringizni kelajakda test <br /> natijasini
-            yuborish uchun foydalanamiz
-          </p>
+      {/* Title */}
+      <h1 className="text-2xl font-semibold">Test boshlash uchun tayyor</h1>
 
-          {/* Form */}
-          <form onSubmit={handleSubmit} className="space-y-3.5">
-            <Input
-              border
-              required
-              size="lg"
-              name="name"
-              label="Ism"
-              value={name}
-              disabled={isLoading}
-              placeholder="Ismingizni kiriting"
-              onChange={(value) => setField("name", value)}
-            />
-
-            <Input
-              border
-              required
-              size="lg"
-              type="tel"
-              name="tel"
-              value={phone}
-              label="Tel raqam"
-              disabled={isLoading}
-              placeholder="+998 (__) ___-__-__"
-              onChange={(value) => setField("phone", value)}
-            />
-
-            <Input
-              border
-              min={7}
-              required
-              max={100}
-              size="lg"
-              name="age"
-              value={age}
-              label="Yosh"
-              type="number"
-              disabled={isLoading}
-              placeholder="Yoshingizni kiriting"
-              onChange={(value) => setField("age", value)}
-            />
-
-            <Button
-              size="lg"
-              disabled={isLoading}
-              className="w-full !mt-5 gap-1.5"
-            >
-              <span>Testni boshlash</span>
-              <ChevronRight size={20} />
-            </Button>
-          </form>
-        </div>
-
-        {/* Loader */}
-        {isLoading && (
-          <div className="bar-loader-secondary absolute bottom-0 inset-x-0 w-full h-1" />
-        )}
-      </div>
+      {/* Button */}
+      <Button size="lg" disabled={isLoading} onClick={handleStartTest}>
+        Testni boshlash{isLoading && "..."}
+      </Button>
     </div>
   );
 };
