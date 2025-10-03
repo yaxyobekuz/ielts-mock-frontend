809bdbf4282d432ad7588083d25a7246224a6eee
yaxyobekuz <pubgn9642@gmail.com>
2025-10-03 19:03:19 +0500

    Add getLetterByIndex helper function

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -31,3 +31,5 @@ export const formatUzPhone = (input) => {
   const m = d.match(/^998(\d{2})(\d{3})(\d{2})(\d{2})$/);
   return m ? `+998 (${m[1]}) ${m[2]}-${m[3]}-${m[4]}` : null;
 };
+
+export const getLetterByIndex = (index) => String.fromCharCode(65 + index);
