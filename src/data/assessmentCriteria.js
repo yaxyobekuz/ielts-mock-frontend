79434042b04046d66c6eadf5bfb77a2b019c4b72
yaxyobekuz <pubgn9642@gmail.com>
2025-10-10 20:06:59 +0500

    Add assessment criteria data for writing and speaking evaluations

---
+++ src/data/assessmentCriteria.js
@@ -0,0 +1,68 @@
+const assessmentCriteria = [
+  {
+    name: "writing",
+    criteria: [
+      [
+        {
+          name: "Task Achievement",
+          key: "taskAchievement",
+        },
+        {
+          name: "Coherence and Cohesion",
+          key: "coherenceAndCohesion",
+        },
+        {
+          name: "Lexical Resource",
+          key: "lexicalResource",
+        },
+        {
+          name: "Grammatical Range and Accuracy",
+          key: "grammaticalRangeAndAccuracy",
+        },
+      ],
+      [
+        {
+          name: "Task Response",
+          key: "taskResponse",
+        },
+        {
+          name: "Coherence and Cohesion",
+          key: "coherenceAndCohesion",
+        },
+        {
+          name: "Lexical Resource",
+          key: "lexicalResource",
+        },
+        {
+          name: "Grammatical Range and Accuracy",
+          key: "grammaticalRangeAndAccuracy",
+        },
+      ],
+    ],
+  },
+  {
+    name: "speaking",
+    criteria: [
+      [
+        {
+          name: "Fluency and Coherence",
+          key: "fluencyAndCoherence",
+        },
+        {
+          name: "Lexical Resource",
+          key: "lexicalResource",
+        },
+        {
+          name: "Grammatical Range and Accuracy",
+          key: "grammaticalRangeAndAccuracy",
+        },
+        {
+          name: "Pronunciation",
+          key: "pronunciation",
+        },
+      ],
+    ],
+  },
+];
+
+export default assessmentCriteria;
