f9a447c0476549a8806a1bc86e589746e3b2a581
yaxyobekuz <pubgn9642@gmail.com>
2025-10-21 08:15:22 +0500

    Enhance RadioGroup component with smooth scrolling and keyboard navigation; improve navigation handling and state updates

--- src/components/questions/RadioGroup.jsx
+++ src/components/questions/RadioGroup.jsx
@@ -1,20 +1,54 @@
+// React
+import { useEffect, useRef } from "react";
+
 // Hooks
 import useStore from "@/hooks/useStore";
+import usePathSegments from "@/hooks/usePathSegments";
+
+// Router
+import { useNavigate, useParams } from "react-router-dom";
 
 const RadioGroup = ({ initialNumber, groups }) => {
+  const navigate = useNavigate();
+  const { questionNumber } = useParams();
+  const { pathSegments: path } = usePathSegments();
   const { updateProperty, getData } = useStore("answers");
   const initialValues = getData();
+
+  const handleNavigate = (newQuestionNumber) => {
+    if (newQuestionNumber === questionNumber) return;
+    navigate(`/test/${path[1]}/${path[2]}/${path[3]}/${newQuestionNumber}`);
+  };
 
   return (
     <ul className="space-y-6">
       {groups.map(({ question, answers }, index) => {
+        const groupNumberRef = useRef(null);
         const groupNumber = initialNumber + index;
         const initialValue = initialValues[groupNumber];
+        const isActive = questionNumber === String(groupNumber);
+
+        useEffect(() => {
+          if (!groupNumberRef) return;
+          if (isActive) {
+            groupNumberRef.current.scrollIntoView({
+              block: "center",
+              behavior: "smooth",
+            });
+          }
+        }, [index, questionNumber]);
 
         return (
           <li key={index}>
             <p className="mb-1">
-              <b className="inline-block py-0.5 px-1.5 rounded mr-2 border-2 transition-colors duration-300">
+              <b
+                tabIndex={0}
+                ref={groupNumberRef}
+                onFocus={() => handleNavigate(groupNumber)}
+                className={`inline-block py-0.5 px-1.5 rounded mr-2 border-2 outline-none transition-colors duration-300 ${
+                  isActive ? "border-blue-400" : "border-gray-200"
+                }`}
+              >
                 {groupNumber}
               </b>
               <span>{question}</span>
@@ -25,7 +59,10 @@ const RadioGroup = ({ initialNumber, groups }) => {
               answers={answers}
               groupNumber={groupNumber}
               initialValue={initialValue?.text}
-              onSelect={(value) => updateProperty(groupNumber, { text: value })}
+              onSelect={(value) => {
+                handleNavigate(groupNumber);
+                updateProperty(groupNumber, { text: value });
+              }}
             />
           </li>
         );
